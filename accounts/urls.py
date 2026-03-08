b30436412211c657c837be21028c430c8fd00f0e
ajmalcoding <ajmal.sde@gmail.com>
2026-03-09 00:54:23 +0530

    Implement designers listing with search and filtering

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('<slug:project_slug>/', views.project_detail_view, name='project_detail_view'),
+    path('designers_listing/', views.designers_list, name='designers_list'),
     path('<str:username>/', views.designer_profile, name='designer_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from accounts.models import DesignerProfile
+from accounts.models import DesignerProfile, Skill
+from categories.models import Category
+from django.db.models import Q
 
 # Create your views here.
 def designer_profile(request, username):
@@ -17,3 +19,36 @@ def designer_profile(request, username):
         'projects_count': projects_count,
     }
     return render(request, "designer_profile.html", context)
+
+def designers_list(request):
+    search = request.GET.get("q")
+    category = request.GET.get("category")
+    selected_skill = request.GET.getlist("skill")
+    location = request.GET.get("location")
+
+    designers = DesignerProfile.objects.all()
+
+    if search:
+        designers = designers.filter(Q(user__username__icontains=search) | Q(name__icontains=search) | Q(location__icontains=search) | Q(categories__name__icontains=search) | Q(title__icontains=search))
+
+    if category:
+        designers = designers.filter(categories__slug=category)
+
+    if selected_skill:
+        designers = designers.filter(skills__slug__in=selected_skill).distinct()
+
+    if location:
+        designers = designers.filter(location__icontains=location)
+
+    categories = Category.objects.all()
+    skills = Skill.objects.all()
+    locations = DesignerProfile.objects.exclude(location="").values_list("location", flat=True).distinct()
+
+    context = {
+        'designers': designers,
+        'skills': skills,
+        'selected_skill':selected_skill,
+        'categories': categories,
+        'locations': locations,
+    }
+    return render(request, "designers_listing.html", context)
